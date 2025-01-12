23ff39938eafbba8d495b4da61b4ef8a2ced0052
AlmasZh <almas.zh22@gmail.com>
2025-01-12 20:00:08 +0500

    added JavaHome environment

--- configuration.nix
+++ configuration.nix
@@ -205,6 +205,7 @@
     QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
     QT_AUTO_SCREEN_SCALE_FACTOR = "1";
     MOZ_ENABLE_WAYLAND = "1";
+		JAVA_HOME = "/nix/var/nix/profiles/default/bin/java";
 
     #WAYLAND_DISPLAY="wayland-1";
     #XDG_RUNTIME_DIR="/run/user/1000";
