8b3356ae1fe5c4d184f6de6152fd81324f36c0ce
AlmasZh <almas.zh22@gmail.com>
2025-10-11 00:27:31 +0500

    add shell alias for kitty's icat

--- home.nix
+++ home.nix
@@ -154,6 +154,7 @@
     shellAliases = {
       c = "clear";
 			l = "eza -la --icons --group-directories-first";
+      icat = "kitty +kitten icat";
       ".." = "cd ..";
       "..." = "cd ../../";
 			"gitlg" = "git log --graph --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%an%C(reset)%C(bold yellow)%d%C(reset) %C(dim white)- %s%C(reset)' --all";
