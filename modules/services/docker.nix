f3fe0eb05fc697e9e6e9099e474c206ca661de87
AlmasZh <almas.zh22@gmail.com>
2025-12-15 15:27:09 +0500

    feat: add daemon settings for docker

--- modules/services/docker.nix
+++ modules/services/docker.nix
@@ -1,11 +1,16 @@
 { config, pkgs, ... }:
 
 {
-  virtualisation.docker.enable = true;
+  virtualisation.docker = {
+    enable = true;
+    daemon.settings = {
+      ipv6 = false;
+      dns = [ "8.8.8.8" "1.1.1.1" ];
+    };
+  };
 
   environment.systemPackages = with pkgs; [
     docker-compose
     # arion
   ];
 }
-
